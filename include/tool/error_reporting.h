6fee96aa42c7b1529468f2fd940e3870619defdd
YarikCompany2 <yaroslavromanenko456@gmail.com>
2026-08-04 22:25:08 +0300

    feat: add error reporting for opengGL

---
+++ include/tool/error_reporting.h
@@ -0,0 +1,20 @@
+#ifndef ERROR_REPORTING_H
+#define ERROR_REPORTING_H
+
+#include <glad/glad.h>
+
+void GLAPIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	unsigned int id,
+	GLenum severity,
+	GLsizei length,
+	const char* message,
+	const void* userParam);
+
+void enableReportGlErrors();
+
+void createErrorFile();
+
+void reportError(const char *message);
+
+#endif // ERROR_REPORTING_H
--- src/display/window.c
+++ src/display/window.c
@@ -1,9 +1,12 @@
 #include "display/window.h"
 
 #include <bw_enums.h>
+#include "tool/error_reporting.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DEBUG
 
 void window_glad_init(uint32_t width, uint32_t height) {
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -20,9 +23,12 @@ void window_framebuffer_size_callback(GLFWwindow *window, int width, int height)
 
 GLFWwindow* window_initialize(uint32_t width, uint32_t height, char title[]) {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef DEBUG
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, BW_BOOLEAN_TRUE);
+    #endif
 
     GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
     if (window == NULL) {
@@ -34,6 +40,16 @@ GLFWwindow* window_initialize(uint32_t width, uint32_t height, char title[]) {
 
     window_glad_init(width, height);
     glfwSetFramebufferSizeCallback(window, window_framebuffer_size_callback);
+
+    #ifdef DEBUG
+    int context_flags = 0;
+    glGetIntegerv(GL_CONTEXT_FLAGS, &context_flags);
+
+    if (context_flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+        printf("Debug context created!\n");
+        enableReportGlErrors();
+    }
+    #endif
 
     return window;
 }
